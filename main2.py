5cb5653657bbcdf18cd9865a1c8e2cab9b6790a9
juliadobroszek <dobroszek.julia@gmail.com>
2025-03-26 16:19:38 +0100

    Uklady_Wstep

---
+++ main2.py
@@ -0,0 +1,10 @@
+# Julia Dobroszek, Malwina Wodnicka 4
+#Metoda iteracyjna Gaussa-Seidla
+
+#Użytkownik wybiera ilość równań (maksymalna ilość może być ograniczona z gówy, ale algorytm uniwersalny)
+
+#Użytkownik podaje warunek stopu (ilość iteracji albo uzyskanie dokładności)
+
+#Wczytywanie współczynników układów równań z plików
+
+#Sprawdzenie, czy podana macierz spełnia warunki zbieżności
